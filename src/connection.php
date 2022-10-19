ae30a978c1a1008b75723b72605128f278e0a13d
Kevin McGruder <lmcgruder@radford.edu>
2022-10-19 19:02:44 -0400

    connection file

---
+++ src/connection.php
@@ -0,0 +1,16 @@
+<?php
+require "../config/config.php"; 
+
+$options = [ 
+    PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    PDO::ATTR_EMULATE_PREPARES => false, 
+];
+
+$dsn = db_type.":host=".db_server.";db_databsename=".db_database.";port=".db_port.";charset=".db_charset; 
+
+try {
+    $pdo = new PDO($dsn, db_user, db_pass, $options); 
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), $e->getCode());    
+} ?>
